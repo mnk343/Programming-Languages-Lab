d11250ee97dbc36312ec90bc4541645b335ca8b3
Mayank Wadhwani <mayank343@gmail.com>
2020-11-28 16:00:32 +0530

    Ques_2 is now complete

--- Haskell/ques_2.hs
+++ Haskell/ques_2.hs
@@ -1,5 +1,6 @@
 import System.Random
 import Data.List
+import System.IO.Unsafe
 
 registered_teams :: [[Char]]
 registered_teams = [ "BS" , "CM" , "CH" , "CV" , "CS" , "DS" , "EE" , "HU" , "MA" , "ME" , "PH" , "ST" ]
@@ -26,20 +27,60 @@ generate_fixture random_shuffle temp_fixture = do
   let element = (head random_shuffle)
   generate_fixture (tail random_shuffle) (temp_fixture ++ [(registered_teams !! element)])
 
+random_shuffle = unsafePerformIO(generate_new_random_shuffling [])
+
+dates = [ "1-12-2020" , "2-12-2020" , "3-12-2020" ]
+times = ["9:30" , "19:30"]
+
+printFixture [] _ = putStrLn ("")
+printFixture fixture number = do
+  let team1 = (head fixture)
+  let team2 = (head (tail fixture))
+  let date = dates !! (div number 2)
+  let time = times !! (mod number 2)
+
+  putStrLn (team1 ++ " vs " ++ team2 ++ "     " ++ date ++ "     " ++ time  )
+  printFixture (tail (tail fixture)) (number+1)
+
+findIndexInFixture :: [[Char]] -> [Char] -> Int -> Int
+findIndexInFixture fixture team_code index = do
+  let element = (head fixture)
+  if (element == team_code)
+    then index
+  else findIndexInFixture (tail fixture) team_code (index+1)
+
+printMatchDetails fixture match_number = do
+  let team1 = fixture !! ( (match_number-1)*2)
+  let team2 = fixture !! (( (match_number-1)*2) + 1)
+  let date = dates !! (div (match_number-1) 2)
+  let time = times !! (mod (match_number-1) 2)
+  putStrLn (team1 ++ " vs " ++ team2 ++ "     " ++ date ++ "     " ++ time  )
+
+printFixtureForTeam fixture team_code = do
+  let index = findIndexInFixture fixture team_code 0
+  let match_number = ((div index 2) + 1)
+  printMatchDetails fixture match_number
+
+printFixtureForDateAndTime fixture match_number = do
+  printMatchDetails fixture match_number
+
 fixture "all" = do
-  random_shuffle <- generate_new_random_shuffling []
-  return (generate_fixture random_shuffle [])
+  let fixture = (generate_fixture random_shuffle [])
+  printFixture fixture 0
 
+fixture team_code = do
+  let fixture = (generate_fixture random_shuffle [])
+  if (length random_shuffle) == 0 then
+    putStrLn "Fixture not created yet"
+  else if team_code `elem` registered_teams then
+    printFixtureForTeam fixture team_code
+  else 
+    putStrLn "Wrong team entered"
 
-
-
-
-
-
-
-
-
-
-
-
-
+nextMatch :: Int -> Double -> IO()
+nextMatch date time = do 
+  let fixture = (generate_fixture random_shuffle [])
+  let match_number_based_on_date = date*2
+  if date==3 && time > 19.30 then putStrLn "No more matches left."
+  else if time > 9.30 then printFixtureForDateAndTime fixture (match_number_based_on_date )
+  else printFixtureForDateAndTime fixture (match_number_based_on_date-1)
