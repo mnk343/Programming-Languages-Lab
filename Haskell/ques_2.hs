bd67dd7288fd212d5920a742d59ce4214dc574e9
Mayank Wadhwani <mayank343@gmail.com>
2020-11-28 14:47:15 +0530

    Working perfectly till now

--- Haskell/ques_2.hs
+++ Haskell/ques_2.hs
@@ -2,13 +2,14 @@ import System.Random
 import Data.List
 
 registered_teams :: [[Char]]
-registered_teams = ["BS" , "CM" , "CH" , "CV" , "CS" , "DS" , "EE" , "HU" , "MA" , "ME" , "PH" , "ST" ]
+registered_teams = [ "BS" , "CM" , "CH" , "CV" , "CS" , "DS" , "EE" , "HU" , "MA" , "ME" , "PH" , "ST" ]
 
 get_random_number :: IO Int
 get_random_number = do 
   n <- randomRIO(0,11)
   return n
 
+generate_new_random_shuffling :: [Int] -> IO [Int]
 generate_new_random_shuffling random_shuffle = 
   do
     index <- get_random_number
@@ -19,9 +20,15 @@ generate_new_random_shuffling random_shuffle =
        generate_new_random_shuffling (random_shuffle ++ [index])
     else return random_shuffle
 
+generate_fixture ::  [Int] -> [[Char]] -> [[Char]]
+generate_fixture [] temp_fixture = temp_fixture
+generate_fixture random_shuffle temp_fixture = do
+  let element = (head random_shuffle)
+  generate_fixture (tail random_shuffle) (temp_fixture ++ [(registered_teams !! element)])
+
 fixture "all" = do
-  let teams_shuffling = generate_new_random_shuffling []
-  teams_shuffling
+  random_shuffle <- generate_new_random_shuffling []
+  return (generate_fixture random_shuffle [])
 
 
 
